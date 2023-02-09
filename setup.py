4263f3120d31308881536626c01494ffcfa5e526
Brian Guarraci <brian.guarraci@gmail.com>
2023-02-08 18:22:46 -0700

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='dataframe-service',
     packages=['dfs'],
-    version='0.1.4',
+    version='0.1.5',
     description='Pandas DataFrame Service (DFS).',
     author='Brian Guarraci',
     license='MIT',
